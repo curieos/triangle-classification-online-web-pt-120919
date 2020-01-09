061a570d77eeecd00ea91364a4c58025acdfa513
Caroline Longnecker <cvrieous@gmail.com>
2020-01-09 09:59:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,7 @@ class Triangle
   
   def kind
     if @a == 0 || @b == 0 || @c == 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      raise TriangleError
     elsif @a == @b && @b == @c
       :equilateral
     elsif @a == @b || @b == @c || @a == @c
